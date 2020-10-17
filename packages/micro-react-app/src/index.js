3e466acca51dd62ad2d3620c3ec8a6565dbbaa8e
Lukasz Zygmanski <lzygmanski@gmail.com>
2020-10-17 21:54:13 +0200

    feat: add webpack config for app
    close #2
    affects: @lzygmanski/micro-react-app, @lzygmanski/micro-react-config

--- packages/micro-react-app/src/index.js
+++ packages/micro-react-app/src/index.js
@@ -1,1 +1,6 @@
-console.log("Works");
+/* eslint react/jsx-filename-extension: "off" */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
