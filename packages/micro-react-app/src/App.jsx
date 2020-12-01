eb259f5c3ea515f1a11ece8db62437899e01175e
Lukasz Zygmanski <lzygmanski@gmail.com>
2020-12-01 23:53:58 +0100

    feat: add react-router-dom
    affects: @lzygmanski/micro-react-app, @lzygmanski/micro-react-module

--- packages/micro-react-app/src/App.jsx
+++ packages/micro-react-app/src/App.jsx
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
-import MicroReactModule from '@lzygmanski/micro-react-module';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import '@lzygmanski/micro-react-module/dist/main.css';
 import './App.css';
 
+const MicroReactModule = lazy(() => import('@lzygmanski/micro-react-module'));
+
 const App = () => (
-  <div className="app">
-    <h1>Hello from micro-rect-app!</h1>
-    <MicroReactModule />
-  </div>
+  <Router>
+    <div className="app">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/module">Module</Link>
+          </li>
+        </ul>
+      </nav>
+      <Suspense fallback={<div>Page is Loading...</div>}>
+        <Switch>
+          <Route path="/module" component={MicroReactModule} />
+          <Route path="/">
+            <h1>Hello from micro-rect-app!</h1>
+          </Route>
+        </Switch>
+      </Suspense>
+    </div>
+  </Router>
 );
 
 export default App;
--- packages/micro-react-module/src/Module.jsx
+++ packages/micro-react-module/src/Module.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './Module.css';
 
 export default function Module() {
-  return <h2 className="module">Hello from micro-react-module</h2>;
+  return <h1 className="module">Hello from micro-react-module</h1>;
 }
